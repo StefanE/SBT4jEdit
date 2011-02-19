02a81909df90e3390dc172c94edef1eb22b03d6c
Stefan K Ettrup <sket@itu.dk>
2011-02-19 09:37:50 +0100

    Simplified color chooser, now its possible to set, normal,information,error and warning colors

--- src/org/scala/sbt/OptionsPane.scala
+++ src/org/scala/sbt/OptionsPane.scala
@@ -15,34 +15,24 @@ import org.gjt.sp.jedit.gui.FontSelector
 import console.gui.Label
 import java.awt.{Component, Color}
 
-class OptionsPane extends AbstractOptionPane("org.scala.sbt.general") with ActionListener {
+class OptionsPane extends AbstractOptionPane("org.scala.sbt.general") {
 
 
   protected override def _init: Unit = {
     var encodings: Array[String] = MiscUtilities.getEncodings(true)
     Arrays.sort(encodings.asInstanceOf[Array[AnyRef]], new MiscUtilities.StringICaseCompare)
-    usejEditBgColor = new JButton("reset")
-    usejEditBgColor.addActionListener(this)
-    addComponent(jEdit.getProperty("options.org.scala.sbt.general.usejEditBgColor"), usejEditBgColor)
-    bgColor = createColorButton("org.scala.sbt.bgColor")
-    addComponent(jEdit.getProperty("options.org.scala.sbt.general.bgColor"), bgColor)
     plainColor = createColorButton("org.scala.sbt.plainColor")
     addComponent(jEdit.getProperty("options.org.scala.sbt.general.plainColor"),plainColor )
-    caretColor = createColorButton("org.scala.sbt.caretColor")
-    addComponent(jEdit.getProperty("options.org.scala.sbt.general.caretColor"),caretColor )
     infoColor = createColorButton("org.scala.sbt.infoColor")
     addComponent(jEdit.getProperty("options.org.scala.sbt.general.infoColor"),infoColor )
     warningColor = createColorButton("org.scala.sbt.warningColor")
     addComponent(jEdit.getProperty("options.org.scala.sbt.general.warningColor"),warningColor )
-
     errorColor = createColorButton("org.scala.sbt.errorColor")
     addComponent(jEdit.getProperty("options.org.scala.sbt.general.errorColor"),errorColor )
   }
 
   protected override def _save: Unit = {
-    jEdit.setColorProperty("org.scala.sbt.bgColor", bgColor.getBackground)
     jEdit.setColorProperty("org.scala.sbt.plainColor", plainColor.getBackground)
-    jEdit.setColorProperty("org.scala.sbt.caretColor", caretColor.getBackground)
     jEdit.setColorProperty("org.scala.sbt.infoColor", infoColor.getBackground)
     jEdit.setColorProperty("org.scala.sbt.warningColor", warningColor.getBackground)
     jEdit.setColorProperty("org.scala.sbt.errorColor", errorColor.getBackground)
@@ -61,17 +51,7 @@ class OptionsPane extends AbstractOptionPane("org.scala.sbt.general") with Actio
     b
   }
 
-  def actionPerformed(e: ActionEvent): Unit = {
-    if (e.getSource != usejEditBgColor) return
-    var c: Color = jEdit.getColorProperty("view.bgColor")
-    jEdit.setColorProperty("org.scala.sbt.bgColor", c)
-    bgColor.setBackground(c)
-  }
-
-  private var bgColor: JButton = null
-  private var usejEditBgColor: JButton = null
   private var plainColor: JButton = null
-  private var caretColor: JButton = null
   private var infoColor: JButton = null
   private var warningColor: JButton = null
   private var errorColor: JButton = null
